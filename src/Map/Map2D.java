862c8fa1ba2cc7568ddd3617e947330cbe6babcf
lishi500 <lishi500@gmail.com>
2016-03-04 16:29:28 +1100

    add getSegment by ID, add untested map clear function

--- src/Map/Map2D.java
+++ src/Map/Map2D.java
@@ -1,9 +1,40 @@
 package Map;
 
+import java.awt.image.BufferedImage;
+
 import Object.Map;
 
 public class Map2D extends Map{
+	final int EMPTY = 0;
+	final int BORDER = -1;
+	final int CONTENT = -2;
 	int[][] imageMatrix;
 	int[][] obstacleMatrix;
 	
+	public void addImageToMap(int xpos,int ypos,BufferedImage img){
+		if(imageMatrix[ypos][xpos]!=0){
+			
+		}
+	}
+	
+	public void cleanImage(int x_index, int y_index){
+		if(imageMatrix[x_index][y_index]>EMPTY){
+			int id = imageMatrix[x_index][y_index];
+			BufferedImage bimg = MapResources.getInstance().getSegmentByID(id);
+			for(int i=x_index;i<bimg.getWidth();i++){
+				for(int j=y_index;j<bimg.getHeight();j++){
+					imageMatrix[i][j]=0;
+				}
+			}
+		}
+		else if(imageMatrix[x_index][y_index]==EMPTY){
+			return;
+		}
+		else if(imageMatrix[x_index][y_index]==BORDER){
+			
+		}
+	}
+	public void clearArea(int x_index,int y_index,int w,int h){
+		
+	}
 }
--- src/Map/MapResources.java
+++ src/Map/MapResources.java
@@ -7,12 +7,16 @@ import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
+import Data.DAO.MapSegmentDAO;
+
 public class MapResources {
 	HashMap<String, BufferedImage> resourceCenter;
+	HashMap<Integer,BufferedImage> segmentPool; 
 	private final String parentPath = "img/resources/";
 	private static MapResources resources;
 	private MapResources(){
 		resourceCenter = new HashMap<String, BufferedImage>();
+		segmentPool = new HashMap<Integer, BufferedImage>();
 	}
 	public static MapResources getInstance(){
 		if(resources==null){
@@ -46,9 +50,25 @@ public class MapResources {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+			}else{
+				return null;
 			}
 		}
 		return bimg;
+	}
+	public BufferedImage getSegmentByID(int id){
+		if(segmentPool.containsKey(id))
+			return segmentPool.get(id);
+		MapSegment mapSegment = new MapSegmentDAO().get(id);
+		BufferedImage bimg = getImageSegment(mapSegment);
+		segmentPool.put(id, bimg);
+		return bimg;
+	}
+	public BufferedImage getImageSegment(MapSegment mapSegment){
+		BufferedImage bimg =  getResources(mapSegment.file_path);
+		if(bimg!=null){
+			return bimg.getSubimage(mapSegment.xpos, mapSegment.ypos, mapSegment.width, mapSegment.height);
+		}
+		return null;
 	}
 }
